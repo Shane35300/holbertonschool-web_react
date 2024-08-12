126028733a1e180c121083c46f68498bfcf4bf34
shane <6874@holbertonstudents.com>
2024-08-12 19:14:38 +0200

    task 2

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,4 +1,3 @@
-console.log('Hello, Webpack!');
 interface Teacher {
 	firstName: string;
 	lastName: string;
@@ -7,3 +6,23 @@ interface Teacher {
 	location: string;
 	[key: string]: any;
 }
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+const teacher3: Teacher = {
+firstName: 'John',
+fullTimeEmployee: false,
+lastName: 'Doe',
+location: 'London',
+contract: false,
+};
+const director1: Directors = {
+firstName: 'John',
+lastName: 'Doe',
+location: 'London',
+fullTimeEmployee: true,
+numberOfReports: 17,
+};
+
+console.log(teacher3);
+console.log(director1);
