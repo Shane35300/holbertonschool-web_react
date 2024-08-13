a68d3de0792477364b82314005cbcbd135b02dfa
shane <6874@holbertonstudents.com>
2024-08-13 08:57:50 +0200

    task 4

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -10,18 +10,18 @@ interface Directors extends Teacher {
 	numberOfReports: number;
 }
 const teacher3: Teacher = {
-firstName: 'John',
-fullTimeEmployee: false,
-lastName: 'Doe',
-location: 'London',
-contract: false,
+	firstName: 'John',
+	fullTimeEmployee: false,
+	lastName: 'Doe',
+	location: 'London',
+	contract: false,
 };
 const director1: Directors = {
-firstName: 'John',
-lastName: 'Doe',
-location: 'London',
-fullTimeEmployee: true,
-numberOfReports: 17,
+	firstName: 'John',
+	lastName: 'Doe',
+	location: 'London',
+	fullTimeEmployee: true,
+	numberOfReports: 17,
 };
 interface printTeacherFunction {
 	(firstName: string, lastName: string): string;
@@ -29,4 +29,28 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
 	return `${firstName[0]}. ${lastName}`;
 }
-console.log(printTeacher("Shane", "Vaudrey"));
+interface StudentConstructor {
+	firstName: string;
+	lastName: string;
+}
+interface StudentClassInterface {
+	workOnHomework(): string;
+	displayName(): string;
+}
+class StudentClass implements StudentClassInterface {
+	firstName: string;
+	lastName: string;
+
+	constructor({ firstName, lastName }: StudentConstructor) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return "Currently working";
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
