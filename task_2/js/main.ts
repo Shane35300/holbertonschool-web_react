4dcf114f3914fa3947b9133f5d30261a7dae5bce
shane <6874@holbertonstudents.com>
2024-08-13 15:55:50 +0200

    task 6

--- task_2/js/main.ts
+++ task_2/js/main.ts
@@ -56,5 +56,13 @@ function executeWork(employee: Director | Teacher): void {
 		console.log(employee.workTeacherTasks());
 	}
 }
-executeWork(createEmployee(200));
-executeWork(createEmployee(1000));
+type Subjects = "Math" | "History";
+function teachClass(todayClass: Subjects): string {
+	if(todayClass === "Math") {
+		return 'Teaching Math';
+	} else {
+		return 'Teaching History';
+	}
+}
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
