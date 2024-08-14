18018dd895efef097f92d05700bfc91198f02b5b
shane <6874@holbertonstudents.com>
2024-08-14 12:27:49 +0200

    task_2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js', // Point d'entrée du fichier JavaScript
+  output: {
+    filename: 'bundle.js', // Nom du fichier JavaScript généré
+    path: path.resolve(__dirname, 'public'), // Dossier de sortie
+  },
+  mode: 'production', // Mode de compilation
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], // Charger les fichiers CSS
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource', // Charger les fichiers d'images
+        generator: {
+          filename: 'images/[name][ext][query]', // Nom du fichier de sortie
+        },
+      },
+    ],
+  },
+};
