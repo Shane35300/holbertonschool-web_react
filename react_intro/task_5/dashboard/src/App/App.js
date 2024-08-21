edfea2268433926faf8a69bff8e39668df1d05f5
shane <6874@holbertonstudents.com>
2024-08-21 16:55:49 +0200

    task10

---
+++ react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,40 @@
+import logo from './holberton-logo.jpg';
+import React, { useEffect } from 'react';
+import favicon from './favicon.ico';
+import { getFullYear, getFooterCopy } from './utils';
+import './App/App.css';
+
+function App() {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = favicon;
+    document.head.appendChild(link);
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="loginForm">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+          <br />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <br />
+          <button type="button">OK</button>
+        </div>
+      </div>
+      <footer className="App-footer">
+        <p>{getFooterCopy(true)}</p>
+        <p>Copyright {getFullYear()} - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Notifications/Notifications.css';
+import { getLatestNotification } from './utils';
+
+/**
+ * Notifications component.
+ * @returns {JSX.Element} - A div with a notification message.
+ */
+export default function Notifications() {
+  const handleButtonClick = () => {
+    console.log('Close button has been clicked');
+  };
+  return (
+    <div className="Notifications">
+      <button
+        style={{ position: 'absolute', top: '10px', right: '10px', background: 'transparent', border: 'none', cursor: 'pointer', fontSize: '20px' }}
+        aria-label="Close"
+        onClick={handleButtonClick}
+      >
+        x
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
+    </div>
+  );
+}
