0b2cca878c6f8154af805af0f6ae7ea303dc120b
shane <6874@holbertonstudents.com>
2024-08-30 11:36:35 +0200

    task_0

--- React_component/task_0/dashboard/src/Notifications/Notifications.js
+++ React_component/task_0/dashboard/src/Notifications/Notifications.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Notifications.css';
 import NotificationItem from './NotificationItem';
-import { getLatestNotification } from '../utils/utils';
 
 /**
  * Notifications component.
