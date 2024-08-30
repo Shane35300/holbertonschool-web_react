eddb9ae75aaf43833907ff23dc2276ba0885e835
shane <6874@holbertonstudents.com>
2024-08-30 11:31:57 +0200

    task 0

--- react_component/task_0/dashboard/src/App/App.js
+++ react_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
@@ -8,48 +8,50 @@ import PropTypes from 'prop-types';
 import favicon from '../favicon.ico';
 import './App.css';
 
-function App({ isLoggedIn }) {
-  useEffect(() => {
+class App extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    isLoggedIn: false,
+  };
+
+  componentDidMount() {
     const link = document.createElement('link');
     link.rel = 'icon';
     link.href = favicon;
     document.head.appendChild(link);
-  }, []);
+  }
 
-  // Création de la liste des cours
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
+  render() {
+    const { isLoggedIn } = this.props;
 
-  // Création de la liste des notifications
-  const listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
-  ];
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
 
-  return (
-    <React.Fragment>
-      <Notifications listNotifications={listNotifications} />
-      <div className="App">
-        <Header />
-        <div className="App-body">
-          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+    ];
+
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          <div className="App-body">
+            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-};
-
-App.defaultProps = {
-  isLoggedIn: false,
-};
 
 export default App;
