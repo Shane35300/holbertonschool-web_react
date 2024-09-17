fcd87f30f5528243405f3930a65a5197d76374ce
shane <6874@holbertonstudents.com>
2024-09-17 10:52:30 +0200

    task1

---
+++ react_redux_reducer_selector/task_1/dashboard/src/uiReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+	// État initial
+  };
+
+  const uiReducer = (state = initialState, action) => {
+	switch (action.type) {
+	  // Gérer les actions ici
+	  default:
+		return state;
+	}
+  };
+
+  export default uiReducer;
